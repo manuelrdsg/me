7e78a96e0a8d5483a46db04d4ffbedaac4953b4b
manuelrdsg <manuelrdsg@gmail.com>
2023-05-08 09:55:22 +0200

    feat: code block component

---
+++ components/CodeBlock/CodeBlock.tsx
@@ -0,0 +1,16 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { darcula, vs } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+// @ts-ignore
+export const CodeBlock = ({ node, inline, className, children, ...props }): any => {
+  const match = /language-(\w+)/.exec(className || '')
+  return !inline && match ? (
+    <SyntaxHighlighter style={darcula} language={match[1]} PreTag="div" {...props}>
+      {String(children).replace(/\n$/, '')}
+    </SyntaxHighlighter>
+  ) : (
+    <code className={className} {...props}>
+      {children}
+    </code>
+  )
+}
