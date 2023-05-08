a1ded7a68d19a824433ca941d80bd3c60b648ed1
manuelrdsg <manuelrdsg@gmail.com>
2023-05-08 09:57:00 +0200

    feat: remove remark as ReactMarkdown is used

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,8 +1,6 @@
 import fs from 'fs'
 import matter from 'gray-matter'
 import path from 'path'
-import { remark } from 'remark'
-import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'content/blog')
 
@@ -54,14 +52,10 @@ export async function getPostData(id: string) {
   // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents)
 
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark().use(html).process(matterResult.content)
-  const contentHtml = processedContent.toString()
-
   // Combine the data with the id and contentHtml
   return {
     id,
-    contentHtml,
+    markdownBody: matterResult.content,
     ...(matterResult.data as { date: string; title: string }),
   }
 }
