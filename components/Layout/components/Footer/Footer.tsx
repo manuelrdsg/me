da78a7379a629500f4f8e81ccae74204a69c7ddd
manuelrdsg <manuelrdsg@gmail.com>
2023-05-22 18:50:33 +0200

    feat: links and footer styles

--- components/Layout/components/Footer/Footer.tsx
+++ components/Layout/components/Footer/Footer.tsx
@@ -1,19 +1,36 @@
-import Link from 'next/link'
+import Link from 'components/Link'
 
 export const Footer = () => {
   return (
-    <footer className={'mx-auto max-w-5xl px-6 w-full'}>
-      <div className={'flex flex-row justify-between py-6 border-t border-secondary dark:border-dark-secondary'}>
+    <footer className={' flex flex-col mx-auto max-w-5xl px-6 w-full space-y-2'}>
+      <div className={'flex flex-row justify-between pt-6 border-t border-secondary dark:border-dark-secondary'}>
         <div className={'flex flex-row space-x-3'}>
-          <Link href="/">Home</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/cv">CV</Link>
+          <Link className={'text-secondary-text no-underline'} href="/">
+            Home
+          </Link>
+          <Link className={'text-secondary-text no-underline'} href="/blog">
+            Blog
+          </Link>
+          <Link className={'text-secondary-text no-underline'} href="/cv">
+            CV
+          </Link>
         </div>
         <div className={'flex flex-row space-x-3'}>
-          <Link href="https://tiles.manuelrdsg.com">Tiles</Link>
-          <Link href="https://twitter.com/manuelrdsg">Twitter</Link>
-          <Link href="https://github.com/manuelrdsg">GitHub</Link>
+          <Link className={'text-secondary-text no-underline'} href="https://tiles.manuelrdsg.com">
+            Tiles
+          </Link>
+          <Link className={'text-secondary-text no-underline'} href="https://twitter.com/manuelrdsg">
+            Twitter
+          </Link>
+          <Link className={'text-secondary-text no-underline'} href="https://github.com/manuelrdsg">
+            GitHub
+          </Link>
         </div>
+      </div>
+      <div className={'grid pb-6'}>
+        <p className={'justify-self-end text-secondary-text text-sm'}>
+          © {new Date().getFullYear()} Manuel Rodríguez-Sánchez Guerra
+        </p>
       </div>
     </footer>
   )
