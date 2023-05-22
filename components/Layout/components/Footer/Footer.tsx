8b5091f94ce8f7d8e6451568528fe659789d1c94
manuelrdsg <manuelrdsg@gmail.com>
2023-05-22 20:31:50 +0200

    style: footer responsiveness

--- components/Layout/components/Footer/Footer.tsx
+++ components/Layout/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className={' flex flex-col mx-auto max-w-5xl px-6 w-full space-y-2'}>
       <div className={'flex flex-row justify-between pt-6 border-t border-secondary dark:border-dark-secondary'}>
-        <div className={'flex flex-row space-x-3'}>
+        <div className={'flex sm:flex-row flex-col sm:space-x-3 sm:space-y-0 space-y-3 '}>
           <Link className={'text-secondary-text no-underline'} href="/">
             Home
           </Link>
@@ -15,7 +15,7 @@ export const Footer = () => {
             CV
           </Link>
         </div>
-        <div className={'flex flex-row space-x-3'}>
+        <div className={'flex sm:flex-row flex-col sm:space-x-3 sm:space-y-0 space-y-3 '}>
           <Link className={'text-secondary-text no-underline'} href="https://tiles.manuelrdsg.com">
             Tiles
           </Link>
