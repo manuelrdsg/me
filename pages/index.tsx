09f63eeaf8de840b79d447284cb72025f7ea16a7
manuelrdsg <manuelrdsg@gmail.com>
2023-07-09 22:12:15 +0200

    feat: home initial implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,50 @@
 import { Post } from 'models/PostModel'
 import { GetStaticProps } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
+import Image from 'next/image'
 
-import Date from 'components/Date'
 import Layout from 'components/Layout'
 
 import { getSortedPostsData } from 'lib/posts'
 
-import utilStyles from 'styles/utils.module.css'
-
-import { siteTitle } from 'utils/strings'
+const profPic = require('styles/test.jpg')
 
 const Home = ({ allPostsData }: { allPostsData: Post[] }) => {
   return (
     <Layout>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <a className={utilStyles.headingLg} href={`/blog`}>
-          Blog
-        </a>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, metadata }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/blog/${id}`}>{metadata.title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={metadata.date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <div className={'flex-1 flex flex-col items-stretch justify-center'}>
+        <div className={'block mx-o w-full md:grid md:grid-cols-2 md:gap-[3.5rem] md:place-content-center'}>
+          <div className={'max-w-xl mx-auto md:mx-0 h-72 md:h-full max-h-[681px] self-center'}>
+            <div
+              className={
+                'border rounded-3xl border-dark-primary block relative overflow-hidden z-1 inset-0 object-center h-full w-full'
+              }>
+              <Image className={'z-0 absolute inset-0 object-cover'} fill src={profPic} alt={'profPic'} />
+            </div>
+          </div>
+          <div
+            className={
+              'max-w-xl mx-auto md:mx-0 py-[--grid-gap] md:py-[calc(var(--grid-gap)+theme(spacing.12))] md:self-center'
+            }>
+            <h1 className={'text-5xl font-bold'}>Hola.</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor tortor sit amet sem rhoncus, ut
+              commodo mauris pharetra. Quisque at dolor arcu. Phasellus aliquam ante vitae convallis accumsan.
+              Pellentesque et turpis diam. Duis varius ullamcorper urna, placerat efficitur ex eleifend et. Morbi
+              molestie faucibus nulla, eu consequat ante hendrerit vel. Ut tempus lectus maximus nibh maximus, a
+              consequat orci consectetur. Fusce nec odio lobortis, pretium enim vel, volutpat orci. Fusce sapien risus,
+              faucibus vel commodo vel, condimentum vel tortor. Duis feugiat imperdiet lacus vel aliquet. Sed tristique
+              sollicitudin augue nec dapibus. Aliquam mollis lacinia posuere. Aliquam non ante eu nisl volutpat pulvinar
+              in ac mi. Ut eget diam a metus semper gravida. Duis mi mi, vehicula ac massa at, rutrum accumsan tortor.
+              Aenean ut pulvinar nisi. Vivamus nec eros ex. Quisque ut efficitur dolor. Integer tristique leo id magna
+              dictum, in euismod dui sollicitudin. Etiam ultricies lacinia rutrum. Quisque volutpat neque sit amet
+              commodo molestie. Nam mattis augue ut diam interdum imperdiet. Proin molestie urna sit amet purus
+              suscipit, in maximus lectus tempor. Donec fermentum convallis lacus ac porttitor. Curabitur lacus orci,
+              convallis sed nibh vitae, cursus ultricies lacus. Integer augue ipsum, mattis eu est id, consequat
+              accumsan augue.
+            </p>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
