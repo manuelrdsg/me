c3ee5dacbba87e0a573f5afec60003196a71df67
manuelrdsg <manuelrdsg@gmail.com>
2024-02-10 00:15:04 +0100

    feat: new code block styles

--- components/CodeBlock/CodeBlock.tsx
+++ components/CodeBlock/CodeBlock.tsx
@@ -4,7 +4,12 @@ import { nord } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 export const CodeBlock = ({ node, inline, className, children, ...props }: any) => {
   const match = /language-(\w+)/.exec(className || '')
   return !inline && match ? (
-    <SyntaxHighlighter style={nord} language={match[1]} PreTag="div" {...props}>
+    <SyntaxHighlighter
+      style={nord}
+      customStyle={{ borderRadius: '0.2rem' }}
+      language={match[1]}
+      PreTag="div"
+      {...props}>
       {String(children).replace(/\n$/, '')}
     </SyntaxHighlighter>
   ) : (
