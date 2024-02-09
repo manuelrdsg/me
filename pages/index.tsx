a94d4e45674ca281da01deee6fbfb68105853465
manuelrdsg <manuelrdsg@gmail.com>
2024-02-09 19:46:57 +0100

    feat: copy and typography

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Post } from 'models/PostModel'
 import { GetStaticProps } from 'next'
+import { Shrikhand } from 'next/font/google'
 import Image from 'next/image'
 
 import Layout from 'components/Layout'
@@ -9,10 +10,13 @@ import { getSortedPostsData } from 'lib/posts'
 
 const profPic = require('public/images/profile.jpeg')
 
+const shrikhand = Shrikhand({ weight: '400', subsets: ['latin'] })
+// const metal = Metal({ weight: '400', subsets: ['latin'] })
+
 const Home = ({ allPostsData }: { allPostsData: Post[] }) => {
   return (
     <Layout>
-      <div className={'md:flex-1 flex items-stretch justify-center'}>
+      <div className={'md:flex-1 flex flex-col items-stretch justify-center'}>
         <div className={'block mx-0 w-full md:grid md:grid-cols-2 md:gap-[3.5rem] justify-center items-center'}>
           <div className="relative max-w-xl mx-auto min-w-screen md:mx-0 block h-72 md:h-[50rem]">
             <div className="relative block border border-dark-primary rounded-xl overflow-hidden shadow-sm dark:shadow-md w-full h-full">
@@ -28,14 +32,19 @@ const Home = ({ allPostsData }: { allPostsData: Post[] }) => {
             </div>
           </div>
           <div className="max-w-xl mx-auto md:mx-0 py-[3.5rem] h-auto md:self-center">
-            <h1 className="text-5xl font-bold">Hola.</h1>
+            <h1 className={`${shrikhand.className} text-4xl`}>hola.</h1>
             <p>
-              I&apos;m Manuel a <span className={'italic'}>software engineer</span>{' '}
+              I&apos;m Manuel, a <span className={'italic'}>software engineer with a love for design</span>{' '}
               <span className={'font-bold'}>
                 <del>living in London, Madrid</del> working remotely.{' '}
               </span>
               I&apos;m currently working as a Lead Mobile Engineer at{' '}
-              <Link href={'https://www.bolster.co/'}>Bolster</Link>, helping the team to create
+              <Link href={'https://www.bolster.co/'}>Bolster</Link>, helping the team to shape beautiful and functional
+              products.{' '}
+              <p className={'mt-2'}>
+                I absolutely love collecting and listening to music, and I’m always carrying a film camera with me to
+                snap some pictures.
+              </p>
             </p>
           </div>
         </div>
