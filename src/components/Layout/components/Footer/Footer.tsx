49ca33a89c68f28a0e5dc3190515ac0626124850
Manuel Rodríguez-Sánchez <manuelrdsg@gmail.com>
2024-09-02 19:53:18 +0100

    untrack test post from git
    fix: comment unadded pages
    
    fix: lint

--- src/components/Layout/components/Footer/Footer.tsx
+++ src/components/Layout/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { CameraIcon, FolderOpenIcon, PencilIcon, UserIcon } from '@heroicons/react/24/outline'
+import { CameraIcon, PencilIcon, UserIcon } from '@heroicons/react/24/outline'
 import { Playfair_Display } from 'next/font/google'
 
 import LinkButton from 'components/LinkButton'
@@ -18,9 +18,10 @@ export const Footer = () => {
             <LinkButton className={'text-secondary-text no-underline'} href="/blog">
               Scribbles
             </LinkButton>
-            <LinkButton className={'text-secondary-text no-underline'} href="/uses">
-              Uses
-            </LinkButton>
+            {/*TODO: Restore when Uses page is added #16*/}
+            {/*<LinkButton className={'text-secondary-text no-underline'} href="/uses">*/}
+            {/*  Uses*/}
+            {/*</LinkButton>*/}
           </div>
           <div className={'flex sm:flex-row flex-col sm:space-x-3 sm:space-y-0 space-y-3 '}>
             <LinkButton className={'text-secondary-text no-underline'} href="https://twitter.com/manuelrdsg">
@@ -42,7 +43,7 @@ export const Footer = () => {
         className={
           'sm:hidden border-t border-t-border-main sticky bottom-0 py-1.5 px-4 mt-0 w-auto bg-transparent z-50 after:absolute after:inset-0 after:-z-50  after:bg-dark-primary/80 after:dark:bg-dark-bg/80 after:backdrop-saturate-[180%] after:backdrop-blur-[20px] after:shadow-lg after:shadow-dark-primary/50 dark:after:shadow-dark-bg/50 md:after:shadow-xl md:after:shadow-dark-primary/50 shadow-md'
         }>
-        <div className={'flex flex-row justify-center max-w-3 items-center py-0 '}>
+        <div className={'flex flex-row justify-center items-center py-0 '}>
           <div className={'flex flex-column justify-between max-w-md px-6 w-full items-center'}>
             <LinkButton
               className={
@@ -68,14 +69,15 @@ export const Footer = () => {
               <CameraIcon className={'xs:h-5 xs:w-5 h-4 w-4'} />
               <div className={`${playfair.className} pt-1`}>Tiles</div>
             </LinkButton>
-            <LinkButton
-              className={
-                'flex flex-col justify-center items-center border-b-transparent border-b-4 border-double hover:border-b-secondary hover:dark:border-dark-secondary text-primary  text-xs  no-underline hover:no-underline'
-              }
-              href="https://tiles.manuelrdsg.com">
-              <FolderOpenIcon className={'xs:h-5 xs:w-5 h-4 w-4'} />
-              <div className={`${playfair.className} pt-1`}>Projects</div>
-            </LinkButton>
+            {/*TODO: Restore when Projects page is added #15*/}
+            {/*<LinkButton*/}
+            {/*  className={*/}
+            {/*    'flex flex-col justify-center items-center border-b-transparent border-b-4 border-double hover:border-b-secondary hover:dark:border-dark-secondary text-primary  text-xs  no-underline hover:no-underline'*/}
+            {/*  }*/}
+            {/*  href="https://tiles.manuelrdsg.com">*/}
+            {/*  <FolderOpenIcon className={'xs:h-5 xs:w-5 h-4 w-4'} />*/}
+            {/*  <div className={`${playfair.className} pt-1`}>Projects</div>*/}
+            {/*</LinkButton>*/}
           </div>
         </div>
       </footer>
