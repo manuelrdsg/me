a665ab870a1abbbc15dc8c734bb619db38f4836b
Manuel Rodríguez-Sánchez <manuelrdsg@gmail.com>
2024-11-10 18:15:07 +0100

    release: 1.1.2

--- src/pages/home/components/ProfileImage.tsx
+++ src/pages/home/components/ProfileImage.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image'
-
-const profPic = require('public/images/profile.jpeg')
+import ProfilePicture from 'public/images/profile.webp'
 
 const ProfileImage = () => {
   return (
     <div className="relative block border border-dark-primary rounded-xl overflow-hidden shadow-sm dark:shadow-md w-full h-full">
       <div className="h-full w-full relative overflow-hidden bg-neutral-100">
         <Image
+          priority
           className="h-full w-full object-cover text-transparent opacity-100 scale-150 grayscale-0 brightness-100 dark:grayscale-15 dark:brightness-90"
           fill
-          src={profPic}
-          alt="profPic"
+          src={ProfilePicture}
+          alt="Profile picture"
           style={{ objectPosition: 'center 30%' }}
         />
       </div>
---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+export enum Themes {
+  DARK = 'dark',
+  LIGHT = 'light',
+}
+
+export const DEFAULT_THEME = Themes.DARK
