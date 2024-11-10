0c86950bcdc5448f196d18c38ebf1c0940051b10
Manuel Rodríguez-Sánchez <manuelrdsg@gmail.com>
2024-11-10 16:30:12 +0000

    feat: update next

--- src/pages/home/components/ProfileImage.tsx
+++ src/pages/home/components/ProfileImage.tsx
@@ -7,6 +7,7 @@ const ProfileImage = () => {
     <div className="relative block border border-dark-primary rounded-xl overflow-hidden shadow-sm dark:shadow-md w-full h-full">
       <div className="h-full w-full relative overflow-hidden bg-neutral-100">
         <Image
+          priority
           className="h-full w-full object-cover text-transparent opacity-100 scale-150 grayscale-0 brightness-100 dark:grayscale-15 dark:brightness-90"
           fill
           src={profPic}
